f2a8c9bf515c192f52a0806111edc8c6d7c110cd
Haolan <coopticks@outlook.com>
2025-03-27 14:14:29 +0800

    fix: append pc path

--- internal/actions/pc/env.go
+++ internal/actions/pc/env.go
@@ -2,9 +2,18 @@ package pc
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 )
 
+func appendPCPath(path string) string {
+	if env, ok := os.LookupEnv("PKG_CONFIG_PATH"); ok {
+		return path + ":" + env
+	}
+	return path
+}
+
 func SetPath(cmd *exec.Cmd, path string) {
-	cmd.Env = append(cmd.Environ(), fmt.Sprintf("PKG_CONFIG_PATH=%s", path))
+	pcPath := fmt.Sprintf("PKG_CONFIG_PATH=%s", appendPCPath(path))
+	cmd.Env = append(os.Environ(), pcPath)
 }
